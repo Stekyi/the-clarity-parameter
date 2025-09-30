883c2993a565a57116cefa16b1a98a0ecd2aeee4
stekyi <samuel_asamoah.tekyi@hsbi.de>
2025-10-01 00:17:40 +0200

    Update clarity_app.py and add new files for web interface

--- clarity_app.py
+++ clarity_app.py
@@ -7,50 +7,77 @@ load_dotenv()
 HUGGINGFACE_API_TOKEN = os.getenv('HUGGINGFACE_API_TOKEN')
 
 
-llm = HuggingFaceEndpoint(repo_id='deepseek-ai/DeepSeek-V3.1-Terminus', task='text-generation',
-                          temperature=0.3,   do_sample=False,     max_new_tokens=20  )
-chatmodel = ChatHuggingFace(llm=llm)
+class Clarity:
+    def __init__(self, question=None):
+        self.question = question
+        self.llm = HuggingFaceEndpoint(repo_id='deepseek-ai/DeepSeek-V3.1-Terminus', task='text-generation',
+                            temperature=0.3,   do_sample=False,     max_new_tokens=20  )
+        self.chatmodel = ChatHuggingFace(llm=self.llm)
+        self.dict_of_whys = {}
+    
+    def __str__(self):
+        return f'you are here to ask a question, you have this {self.question}'
 
 
-mainQuestion = ''
-followUp = ''
-dict_of_whys = {}
+    def getAnswers(self, followUpQuestion = None, questionNumber=0):
 
+        self.followUpQuestion = followUpQuestion
 
-for i in range(0,5):
-    if i == 0:
+        if not self.followUpQuestion:
+            self.followUpQuestion = self.question
         
-        mainQuestion = input('ask ur question:..  ')
-        template = 'Answer in one sentence: {question} ? Please give very short one sentence summary as answer.'
-        prompt = PromptTemplate(input_variables=['question'], template=template)
-        chain = prompt | chatmodel
-        response = chain.invoke({'question': mainQuestion})
-        print(response.content)
+        
+        if questionNumber == 0:
+            #self.dict_of_whys = {}
 
-        dict_of_whys[mainQuestion] =  response.content
+            template = 'Answer in one sentence: {question} ? Please give very short one sentence summary as answer.'
+            prompt = PromptTemplate(input_variables=['question'], template=template)
+            chain = prompt | self.chatmodel
+            response = chain.invoke({'question': self.followUpQuestion})
+                
+            self.dict_of_whys[self.followUpQuestion] =  response.content
+            return response.content
 
-    else:
-        followUp = input(f' Ask your {i} follow-up')
-        template = '\na follow up question to you answer is:.. {followq}. Please give very short one sentence summary as answer.'
-        prompt = PromptTemplate(input_variables=['followq'], template=template)
-        chain = prompt | chatmodel
-        response = chain.invoke({'followq':followUp })
-        print(response.content)
+        else:
+                
+            template = '\n a follow up question to you answer is:.. {followq}. Please give very short one sentence summary as answer.'
+            prompt = PromptTemplate(input_variables=['followq'], template=template)
+            chain = prompt | self.chatmodel
+            response = chain.invoke({'followq':self.followUpQuestion })
+                
+            self.dict_of_whys[self.followUpQuestion] =  response.content
+            return response.content
 
-        dict_of_whys[followUp] =  response.content
-      
+            
 
-template = '''based on the question and the follow up and answers given as shown in the dictionary {whys}, the questions are 
-            the keys and the answers as the values, use the 5 whys, 
-            to list a summary of the 5 questions asked and their short answer. 
-            for each of the answers, give 3 academic papers that has researched the question and concluded with the answer as valid for
-            the question.
-            and give a statement of the final problem discovered as a result of the  5 questions. let it be a short statement '''
+    def getSummary(self):
+        
+        template = '''based on the question and the follow up and answers given as shown in the dictionary {whys}, the questions are 
+                the keys and the answers as the values, use the 5 whys, 
+                to list a summary of the 5 questions asked and their short answer. 
+                for each of the answers, give 3 academic papers that has researched the question and concluded with the answer as valid for
+                the question.
+                and give a statement of the final problem discovered as a result of the  5 questions. let it be a short statement '''
 
-prompt = PromptTemplate(input_variables=['whys'], template = template)
+        prompt = PromptTemplate(input_variables=['whys'], template = template)
 
-chain = prompt | chatmodel
+        chain = prompt | self.chatmodel
+        response = chain.invoke({'whys': self.dict_of_whys})
+        return response.content
 
-response = chain.invoke({'whys': dict_of_whys})
 
-print(response.content)
+if __name__ == '__main__':
+    clarity =  Clarity('why do we get old')
+    
+    clarity.getAnswers(questionNumber= 0)
+    print(clarity.dict_of_whys)
+    clarity.getAnswers(followUpQuestion='why cant we stop growing', questionNumber= 1)
+    print(clarity.dict_of_whys)
+    clarity.getAnswers(followUpQuestion='why cant we reverse ageing', questionNumber= 2)
+    print(clarity.dict_of_whys)
+    print(clarity.getSummary())
+    
+
+
+    
+
---
+++ main.py
@@ -0,0 +1,108 @@
+import streamlit as st
+import os
+from clarity_app   import Clarity
+import time
+
+st.set_page_config(layout='wide')
+st.title('Your Clarity App')
+st.subheader('This app, the clarity app, is to assist people generate ideas for business') 
+st.divider()
+
+
+container1 = st.container()
+st.divider()
+container2 = st.container()
+
+
+
+if 'clarity' not in st.session_state:
+    st.session_state['clarity'] = Clarity()
+
+clarity = st.session_state['clarity']
+
+
+with container2:
+    summary_placeholder = st.empty()                    
+
+
+with container1:
+    col_left, col_center = st.columns(2)
+
+
+    with col_left:
+        st.subheader(f'Answer(s) on {len(clarity.dict_of_whys)} ')                     
+        
+        for key in clarity.dict_of_whys:
+            st.subheader(key)
+            st.write(clarity.dict_of_whys[key])
+    
+    
+        
+    with col_center:
+        st.image(os.path.join(os.getcwd(), "static","idea_machine.jpg")) 
+
+        form_values = {
+            'question': None,
+            }
+        
+        left_placeholder = st.empty()
+        
+        st.divider()
+
+        
+        updateCount = st.checkbox(label='Update Count')
+                        
+        form_container = st.container()
+
+        if updateCount:
+            st.write(f'Question {len(clarity.dict_of_whys)}')
+        
+
+            with form_container:
+                
+                with st.form(key='question', clear_on_submit=True):
+                
+                    if len(clarity.dict_of_whys) != 5:
+
+                            form_values['question'] = st.text_input('Enter your  Question:  ')
+                            #print(form_values['question'])
+                            submission = st.form_submit_button(label='Submit Question')
+                            
+                            if submission:
+                                if not all(form_values.values()):
+                                    st.warning('Please enter a question before submitting')
+                            
+                                else:
+                                    st.success('Successfully submitted')
+                                    
+                                    with st.spinner('analysing, please wait!!!'):
+                                        response = clarity.getAnswers(form_values['question'])
+                                        form_container.empty()                            
+                                        left_placeholder.write(response)
+
+                                    
+                            
+                                    
+                    else:
+                        st.subheader('All 5 questions sent and answered')
+                        summary = st.form_submit_button(label='Generate a summary')
+                        reset = st.form_submit_button(label='Start a new Session')
+                        if summary:
+                            problem = clarity.getSummary()
+                            summary_placeholder.write(problem)
+                        if reset:
+                            clarity.dict_of_whys = {}
+                            summary_placeholder.write('')
+                            left_placeholder.write('')
+
+
+st.markdown(
+    """
+    <style>
+    .block-container {
+        padding-top: 2.0rem;  /* default is ~6rem */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
